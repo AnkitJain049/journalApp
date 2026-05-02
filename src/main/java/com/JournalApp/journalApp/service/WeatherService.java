12efe0fe4774bcc06ddba4bf069d7547498cf3fb
Ankit Jain <ankitjain@example.com>
2026-05-02 08:51:22 +0530

    Env vars set, secrets guarded

--- src/main/java/com/JournalApp/journalApp/service/WeatherService.java
+++ src/main/java/com/JournalApp/journalApp/service/WeatherService.java
@@ -1,6 +1,7 @@
 package com.JournalApp.journalApp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,8 @@ import com.JournalApp.journalApp.apiResponse.WeatherResponse;
 @Component
 public class WeatherService {
     
-    private static final String apiKey = "0468b2819bb83b2213c859532a0d25af";
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -18,9 +20,9 @@ public class WeatherService {
     private static final String url = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
     
     public WeatherResponse getWeather(String city){
-        String finalApi=url.replace("CITY", city).replace("API_KEY",apiKey);
+        String finalApi = url.replace("CITY", city).replace("API_KEY", apiKey);
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
-        WeatherResponse body= response.getBody();
+        WeatherResponse body = response.getBody();
         return body;
     }
     
